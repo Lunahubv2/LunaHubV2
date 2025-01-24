308865d2f6ef67dce44b9712501fba3da2eac6f5
Lunahub <jessajealflores02@gmail.com>
2025-01-24 21:22:29 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,12 +1,12 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
-if PlaceId == 2753915549 then
+if PlaceId == 16732694052 then
     -- Fisch
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/Script-Blox/Script/main/Script-Blox")()
-elseif placeId then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/jessajeal02/LunaHubV2/refs/heads/main/Local/Fisch.lua")()
+elseif placeId == 0000000 then
     -- Rivals
     loadstring(game:HttpGet"https://raw.githubusercontent.com/Script-Blox/Script/main/Script-Blox")()
-elseif placeId then
+elseif placeId == 0000000 then
     -- Arsenal
     loadstring(game:HttpGet"https://raw.githubusercontent.com/Script-Blox/Script/main/Script-Blox")() -- 
 end
