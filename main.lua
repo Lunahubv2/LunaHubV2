f400dd3be73f9c3878d2240f141743b6b3e8a4c4
Lunahub <jessajealflores02@gmail.com>
2025-01-24 21:18:49 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -406,7 +406,7 @@ task.spawn(function()
                          
             local Verify = verifyKey(TextBox.Text)
             if Verify then
-                loadstring(game:HttpGet("https://pastebin.com/raw/DTrES0c6"))()       
+                loadstring(game:HttpGet"https://raw.githubusercontent.com/jessajeal02/LunaHubV2/main/source.lua")()      
             else 
             end 
         end	
