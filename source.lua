cd22e1083c31f038bdaa96e15bf0ceb70d1223a7
Lunahub <jessajealflores02@gmail.com>
2025-01-28 11:22:29 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,6 +1,9 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
-if PlaceId == 16732694052 then
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+    -- Blox Fruit
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/Lunahubv2/LunaHubV2/refs/heads/main/Local/Fisch.lua")()
+elseif PlaceId == 16732694052 then
     -- Fisch
     loadstring(game:HttpGet"https://raw.githubusercontent.com/Lunahubv2/LunaHubV2/refs/heads/main/Local/Fisch.lua")()
 elseif placeId == 0000000 then
