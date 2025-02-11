53386b6a1cde4d128d1b8c9246bc5cbd6abd36d0
Lunahub <176980689+Lunahubv2@users.noreply.github.com>
2025-02-11 20:43:18 +0800

    Update status.lua

--- status.lua
+++ status.lua
@@ -1,14 +1,57 @@
--- Function to send notification
+repeat wait(1) until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+
 local function sendNotification()
     game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Luna Hub V2", -- Required
-        Text = "The Key is Invalid. Please Try Again!", -- Required
+        Title = "Blox Fruit ", -- Required
+        Text = "Script Working Properly", -- Required
         Icon = "rbxassetid://1234567890" -- Optional
     })
     
-    -- Wait for a few seconds before closing (this does not actually close the notification)
-    wait(5) -- Adjust the number of seconds as needed
+    wait(10) -- Adjust the number of seconds as needed
 end
 
--- Call the function to send the notification
 sendNotification()
+elseif PlaceId == 16732694052 then
+        -- Fisch
+    local function sendNotification()
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Fisch -- Online", -- Required
+        Text = "Script Working Properly", -- Required
+        Icon = "rbxassetid://1234567890" -- Optional
+    })
+    
+    wait(10) -- Adjust the number of seconds as needed
+end
+
+sendNotification()
+elseif PlaceId == 17625359962 then
+        -- Rivals
+    local function sendNotification()
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Rivals -- Offline ", -- Required
+        Text = "Script Working Properly", -- Required
+        Icon = "rbxassetid://1234567890" -- Optional
+    })
+    
+    wait(10) -- Adjust the number of seconds as needed
+end
+
+sendNotification()
+elseif PlaceId == 0000000 then
+        
+    local function sendNotification()
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Blox Fruit ", -- Required
+        Text = "Script Working Properly", -- Required
+        Icon = "rbxassetid://1234567890" -- Optional
+    })
+    
+    wait(10) -- Adjust the number of seconds as needed
+end
+
+sendNotification()
+else
+    game.Players.LocalPlayer:Kick("Sorry this games is not supported. Supported Game")
+end
