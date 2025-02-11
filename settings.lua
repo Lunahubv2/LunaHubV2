506a0d80f3ebeb4a36914754e9e29246503617cd
Lunahub <176980689+Lunahubv2@users.noreply.github.com>
2025-02-11 10:47:04 +0800

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -1,21 +1,6 @@
-local player = game.Players.LocalPlayer
-local teams = game:GetService("Teams")
-
--- Change "Pirates" to "Marines" if you want to join Marines instead
-local desiredTeam = "Marines"
-
--- Function to join a team
-local function joinTeam()
-    if teams:FindFirstChild(desiredTeam) then
-        player.Team = teams[desiredTeam]
-        print("Joined team: " .. desiredTeam)
-    else
-        print("Team does not exist!")
-    end
-end
-
--- Wait for the game to load
-game.Players.PlayerAdded:Wait()
-joinTeam()
+local Settings = {
+  JoinTeam = "Marines"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
  
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Lunahubv2/LunaHubV2/refs/heads/main/Local/Bloxfruit.lua"))
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Lunahubv2/LunaHubV2/refs/heads/main/Local/Bloxfruit.lua"))(Settings)
